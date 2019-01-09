e4ebd74a245c06731be2be2e44ab8ae47d9d7d13
saranyaravichander <saranyaravichander@live.com>
2019-01-08 23:59:06 -0500

    Initial commit

---
+++ ConfigUpdater/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Configuration;
+
+namespace ConfigUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uniqueValue = ConfigurationSettings.AppSettings["aliasOrUniquevalue"];
+
+            // here, you can now decide what to do - for demo purposes,
+            // I just set the ID value to a fixed value if it was empty before
+            if (!string.IsNullOrEmpty(uniqueValue))
+            {
+                string json = File.ReadAllText(@"..\..\..\ProvisioningProject\azuredeploy.parameters.json");
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                jsonObj["parameters"]["hubName"]["value"] = "iothub" + uniqueValue;
+                jsonObj["parameters"]["storageName"]["value"] = "storage" + uniqueValue;
+                jsonObj["parameters"]["appServiceName"]["value"] = "webjob" + uniqueValue;
+                jsonObj["parameters"]["appServicePlanName"]["value"] = "appserviceplan" + uniqueValue;
+                jsonObj["parameters"]["resourceGroupName"]["value"] = "labrg" + uniqueValue;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(@"..\..\..\ProvisioningProject\azuredeploy.parameters.json", output);
+            }
+
+        }
+    }
+}
